04f61dccc62f5004997b184c90fb9142943dbdb7
ynncstslv <ynncstslv@gmail.com>
2023-05-29 19:41:23 -0300

    feat: login and logout functionality

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -5,9 +5,17 @@ import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import { User } from '@prisma/client';
+import { signOut } from 'next-auth/react';
 
-const UserMenu = () => {
+interface UserMenuProps {
+	currentUser?: User | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 	const registerModal = useRegisterModal();
+	const loginModal = useLoginModal();
 	const [isOpen, setIsOpen] = useState(false);
 
 	const toggleOpen = useCallback(() => {
@@ -36,10 +44,21 @@ const UserMenu = () => {
 			{isOpen && (
 				<div className="absolute rounded-xl shadow-md w-[48vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
 					<div className="flex flex-col cursor-pointer">
-						<>
-							<MenuItem onClick={() => {}} label="Login" />
-							<MenuItem onClick={registerModal.onOpen} label="Sign Up" />
-						</>
+						{currentUser ? (
+							<>
+								<MenuItem onClick={() => {}} label="My Account" />
+								<MenuItem onClick={() => {}} label="My Favorites" />
+								<MenuItem onClick={() => {}} label="My Jobs" />
+								<MenuItem onClick={() => {}} label="Post a Job" />
+								<hr />
+								<MenuItem onClick={() => signOut()} label="Logout" />
+							</>
+						) : (
+							<>
+								<MenuItem onClick={loginModal.onOpen} label="Login" />
+								<MenuItem onClick={registerModal.onOpen} label="Sign Up" />
+							</>
+						)}
 					</div>
 				</div>
 			)}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import Navbar from './components/navbar/Navbar';
 import './globals.css';
 import { Nunito } from 'next/font/google';
 import ToastProvider from './providers/ToasterProvider';
+import LoginModal from './components/modals/LoginModal';
+import getCurrentUser from './actions/getCurrentUser';
 
 const nunito = Nunito({ subsets: ['latin'] });
 
@@ -13,18 +15,20 @@ export const metadata = {
 		'DevJobs makes it easy for developers to discover and apply for their next career move.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const currentUser = await getCurrentUser();
 	return (
 		<html lang="en">
 			<body className={nunito.className}>
 				<ClientOnly>
 					<ToastProvider />
+					<LoginModal />
 					<RegisterModal />
-					<Navbar />
+					<Navbar currentUser={currentUser} />
 				</ClientOnly>
 				{children}
 			</body>
