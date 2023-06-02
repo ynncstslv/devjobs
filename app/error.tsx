80754e26f78e2b4b73b004c40608561261679220
ynncstslv <ynncstslv@gmail.com>
2023-06-02 13:13:29 -0300

    feat: error state

---
+++ app/error.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { FC, useEffect } from 'react';
+
+import EmptyState from './components/EmptyState';
+
+interface ErrorStateProps {
+	error: Error;
+}
+
+const ErrorState: FC<ErrorStateProps> = ({ error }) => {
+	useEffect(() => {
+		console.error(error);
+	}, [error]);
+
+	return <EmptyState title="Ops..." subtitle="Something went wrong" />;
+};
+
+export default ErrorState;
