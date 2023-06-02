f79e2fd8d9ae1360eb1442d70cb4ed048357cd69
ynncstslv <ynncstslv@gmail.com>
2023-06-02 13:09:59 -0300

    feat: loading state

---
+++ app/Loader.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { PuffLoader } from 'react-spinners';
+
+const Loader = () => {
+	return (
+		<div className="h-[70vh] flex flex-col items-center justify-center">
+			<PuffLoader size={100} color="blue" />
+		</div>
+	);
+};
+
+export default Loader;
